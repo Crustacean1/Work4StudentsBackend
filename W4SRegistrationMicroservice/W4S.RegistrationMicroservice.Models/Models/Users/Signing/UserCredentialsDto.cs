612e8e4074809e6727968ed734988c2992aac97c
Crustacean1 <kamilkowalski314@gmail.com>
2023-01-04 19:45:13 +0100

    feat: working communication with Registration service

--- W4SRegistrationMicroservice/W4S.RegistrationMicroservice.Models/Models/Users/Signing/UserCredentialsDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace W4S.RegistrationMicroservice.Models.Users.Signing
-{
-    public class UserCredentialsDto
-    {
-        public string EmailAddress { get; set; }
-        public string Password { get; set; }
-    }
-}
