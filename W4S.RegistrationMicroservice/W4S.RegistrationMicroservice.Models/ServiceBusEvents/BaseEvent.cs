1778b3b8182339887b32b20b2dbc2b1f683423fe
kochamdziekana <czechumennakompie@gmail.com>
2023-01-10 18:37:56 +0100

    Added Events to Models nuget package.

---
+++ W4S.RegistrationMicroservice/W4S.RegistrationMicroservice.Models/ServiceBusEvents/BaseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4S.RegistrationMicroservice.Models.ServiceBusEvents
+{
+    public class BaseEvent
+    {
+        public DateTime Date { get; set; }
+    }
+}
